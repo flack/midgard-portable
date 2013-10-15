2698a9c39c7bcf3871c2bddb5cad64f5534e16cf
root <root@Omega.(none)>
2013-10-15 15:36:40 +0200

    added tests for querybuilder

--- test/api/query/builderTest.php
+++ test/api/query/builderTest.php
@@ -80,4 +80,122 @@ class midgard_query_builderTest extends testcase
         $this->assertEquals(1, count($results));
         $this->assertEquals($topic->id, $results[0]->id);
     }
+    
+    public function test_limit()
+    {
+        $classname = self::$ns . '\\midgard_topic';
+        
+        $topic = new $classname;
+        $topic->name = __FUNCTION__ . 'testOne';
+        $topic->create();
+        
+        $topic2 = new $classname;
+        $topic2->name = __FUNCTION__ . 'testTwo';
+        $topic2->create();
+        
+        $topic3 = new $classname;
+        $topic3->name = __FUNCTION__ . 'testThree';
+        $topic3->create();
+        
+        $qb = new \midgard_query_builder($classname);
+        $qb->set_limit(1);
+        
+        $results = $qb->execute();
+        
+        $this->assertEquals(1 , count($results));
+    }
+    
+    public function test_grouping()
+    {
+        $classname = self::$ns . '\\midgard_topic';
+        $extra = 'buildertest';
+        $topic = new $classname;
+        $topic->name = __FUNCTION__ . 'testOne';
+        $topic->extra = $extra;
+        $topic->create();
+        
+        $topic2 = new $classname;
+        $topic2->name = __FUNCTION__ . 'testTwo';
+        $topic2->extra = $extra;
+        $topic2->create();
+        
+        $topic3 = new $classname;
+        $topic3->name = __FUNCTION__ . 'testThree';
+        $topic3->extra = $extra;
+        $topic3->create();
+        
+        //should return no results
+        $qb = new \midgard_query_builder($classname);
+        $qb->add_constraint('name', '=', $topic->name);
+        $qb->add_constraint('name', '=', $topic2->name);
+        
+        $results = $qb->execute();
+        $this->assertEquals(0 , count($results));
+        
+        //should return topic + topic2
+        $qb = new \midgard_query_builder($classname);
+        $qb->begin_group('OR');
+            $qb->add_constraint('name', '=', $topic->name);
+            $qb->add_constraint('name', '=', $topic2->name);
+        $qb->end_group();
+        
+        $results = $qb->execute();
+        $this->assertEquals(2 , count($results));
+        
+        //should only return topic
+        $qb = new \midgard_query_builder($classname);
+        $qb->begin_group('OR');
+            $qb->add_constraint('name', '=', $topic->name);
+            $qb->begin_group('AND');
+                $qb->add_constraint('name', '=', $topic2->name);
+                $qb->add_constraint('name', '=', $topic3->name);
+            $qb->end_group();
+        $qb->end_group();
+        
+        $results = $qb->execute();
+        $this->assertEquals(1, count($results));
+        
+        //should only return topic2
+        $qb = new \midgard_query_builder($classname);
+        $qb->begin_group('OR');
+            $qb->add_constraint('name', '=', $topic->name);
+            $qb->add_constraint('name', '=', $topic2->name);
+        $qb->end_group();
+        $qb->begin_group('OR');
+            $qb->add_constraint('name', '=', $topic2->name);
+            $qb->add_constraint('name', '=', $topic3->name);
+        $qb->end_group();
+        
+        $results = $qb->execute();
+        
+        $this->assertEquals(1, count($results));
+        
+        //should return topic + topic2
+        $qb = new \midgard_query_builder($classname);
+        $qb->begin_group('OR');
+            $qb->add_constraint('name', '=' , $topic->name);
+            $qb->begin_group('AND');
+                $qb->add_constraint('name' , '=' , $topic2->name);
+                $qb->add_constraint('extra' , '=', $extra);
+            $qb->end_group();
+        $qb->end_group();
+        
+        $results = $qb->execute();
+
+        $this->assertEquals(2, count($results));
+        
+        //should return topic3+topic2
+        $qb = new \midgard_query_builder($classname);
+        $qb->begin_group('AND');
+            $qb->add_constraint('extra', '=' , $extra);
+            $qb->begin_group('OR');
+                $qb->add_constraint('name' , '=' , $topic2->name);
+                $qb->add_constraint('name' , '=' , $topic3->name);
+            //$qb->end_group();
+        //$qb->end_group();
+        
+        $results = $qb->execute();
+        
+        $this->assertEquals(2, count($results));
+    }
 }
