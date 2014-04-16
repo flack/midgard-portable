7b7955a4e88d26b21c0665272c7bc79eb8df5ae2
flack <flack@contentcontrol-berlin.de>
2014-04-17 00:10:49 +0200

    Add possibility to load different db config by environment variable for test runs

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,10 +2,17 @@
 use midgard\portable\storage\connection;
 
 require_once dirname(__DIR__) . "/vendor/autoload.php";
-
-$db_config = array
-(
-    'memory' => true,
-    'driver' => 'pdo_sqlite'
-);
+$db = getenv('DB');
+if (!empty($db))
+{
+    $db_config = require __DIR__ . DIRECTORY_SEPARATOR . $db . '.inc';
+}
+else
+{
+    $db_config = array
+    (
+        'memory' => true,
+        'driver' => 'pdo_sqlite'
+    );
+}
 connection::initialize($driver, $db_config);
