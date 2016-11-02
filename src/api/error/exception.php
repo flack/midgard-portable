8e1bd01fc4169077cb66afac7f9f801c9ee81d1a
flack <flack@contentcontrol-berlin.de>
2016-11-02 10:56:09 +0100

    remove some duplication

--- src/api/error/exception.php
+++ src/api/error/exception.php
@@ -41,6 +41,37 @@ class exception extends base_exception
     const TREE_IS_CIRCULAR = -26;
     const OBJECT_IS_LOCKED = -27;
 
+    private static $messages = array
+    (
+        self::OK => "MGD_ERR_OK",
+        self::ACCESS_DENIED => "Access Denied.",
+        self::NO_METADATA => "Metadata class not defined.",
+        self::NOT_OBJECT => "Not Midgard Object.",
+        self::NOT_EXISTS => "Object does not exist.",
+        self::INVALID_NAME => "Invalid characters in object's name.",
+        self::DUPLICATE => "Object already exist.",
+        self::HAS_DEPENDANTS => "Object has dependants.",
+        self::RANGE => "Date range error.",
+        self::NOT_CONNECTED => "Not connected to the Midgard database.",
+        self::SG_NOTFOUND => "Sitegroup not found.",
+        self::INVALID_OBJECT => "Object not registered as Midgard Object.",
+        self::QUOTA => "Quota limit reached.",
+        self::INTERNAL => "Critical internal error. ",
+        self::OBJECT_NAME_EXISTS => "Object with such name exists in tree.",
+        self::OBJECT_NO_STORAGE => "Storage table not defined for object.",
+        self::OBJECT_NO_PARENT => "Parent object in tree not defined.",
+        self::INVALID_PROPERTY_VALUE => "Invalid property value.",
+        self::INVALID_PROPERTY => "Invalid property.",
+        self::USER_DATA => "",
+        self::OBJECT_DELETED => "Object deleted.",
+        self::OBJECT_PURGED => "Object purged.",
+        self::OBJECT_EXPORTED => "Object already exported.",
+        self::OBJECT_IMPORTED => "Object already imported.",
+        self::MISSED_DEPENDENCE => "Missed dependence for object.",
+        self::TREE_IS_CIRCULAR => "Circular reference found in object's tree.",
+        self::OBJECT_IS_LOCKED => "Object is locked",
+    );
+
     public function __construct($message = "Undefined error", $code = self::ERROR, base_exception $previous = null)
     {
         midgard_connection::get_instance()->set_error($code);
@@ -50,102 +81,103 @@ class exception extends base_exception
 
     public static function ok()
     {
-        return new self("MGD_ERR_OK", self::OK);
+        return new self(self::$messages[self::OK], self::OK);
     }
 
     public static function access_denied()
     {
-        return new self("Access Denied.", self::ACCESS_DENIED);
+        return new self(self::$messages[self::ACCESS_DENIED], self::ACCESS_DENIED);
     }
 
     public static function no_metadata()
     {
-        return new self("Metadata class not defined.", self::NO_METADATA);
+        return new self(self::$messages[self::NO_METADATA], self::NO_METADATA);
     }
 
     public static function not_object()
     {
-        return new self("Not Midgard Object.", self::NOT_OBJECT);
+        return new self(self::$messages[self::NOT_OBJECT], self::NOT_OBJECT);
     }
 
     public static function not_exists()
     {
-        return new self("Object does not exist.", self::NOT_EXISTS);
+        return new self(self::$messages[self::NOT_EXISTS], self::NOT_EXISTS);
     }
 
     public static function invalid_name()
     {
-        return new self("Invalid characters in object's name.", self::INVALID_NAME);
+        return new self(self::$messages[self::INVALID_NAME], self::INVALID_NAME);
     }
 
     public static function duplicate()
     {
-        return new self("Object already exist.", self::DUPLICATE);
+        return new self(self::$messages[self::DUPLICATE], self::DUPLICATE);
     }
 
     public static function has_dependants()
     {
-        return new self("Object has dependants.", self::HAS_DEPENDANTS);
+        return new self(self::$messages[self::HAS_DEPENDANTS], self::HAS_DEPENDANTS);
     }
 
     public static function range()
     {
-        return new self("Date range error.", self::RANGE);
+        return new self(self::$messages[self::RANGE], self::RANGE);
     }
 
     public static function not_connected()
     {
-        return new self("Not connected to the Midgard database.", self::NOT_CONNECTED);
+        return new self(self::$messages[self::NOT_CONNECTED], self::NOT_CONNECTED);
     }
 
     public static function sg_notfound()
     {
-        return new self("Sitegroup not found.", self::SG_NOTFOUND);
+        return new self(self::$messages[self::SG_NOTFOUND], self::SG_NOTFOUND);
     }
 
     public static function invalid_object()
     {
-        return new self("Object not registered as Midgard Object.", self::INVALID_OBJECT);
+        return new self(self::$messages[self::INVALID_OBJECT], self::INVALID_OBJECT);
     }
 
     public static function quota()
     {
-        return new self("Quota limit reached.", self::QUOTA);
+        return new self(self::$messages[self::QUOTA], self::QUOTA);
     }
 
     public static function internal(base_exception $exception)
     {
-        connection::log()->critical("Internal error", array('exception' => $exception));
-        return new self("Critical internal error. " . $exception->getMessage(), self::INTERNAL, $exception);
+        $message = self::$messages[self::INTERNAL];
+        connection::log()->critical($message, array('exception' => $exception));
+        return new self($message . '. ' . $exception->getMessage(), self::INTERNAL, $exception);
     }
 
     public static function object_name_exists()
     {
-        return new self("Object with such name exists in tree.", self::OBJECT_NAME_EXISTS);
+        return new self(self::$messages[self::OBJECT_NAME_EXISTS], self::OBJECT_NAME_EXISTS);
     }
 
     public static function object_no_storage()
     {
-        return new self("Storage table not defined for object.", self::OBJECT_NO_STORAGE);
+        return new self(self::$messages[self::OBJECT_NO_STORAGE], self::OBJECT_NO_STORAGE);
     }
 
     public static function object_no_parent()
     {
-        return new self("Parent object in tree not defined.", self::OBJECT_NO_PARENT);
+        return new self(self::$messages[self::OBJECT_NO_PARENT], self::OBJECT_NO_PARENT);
     }
 
     public static function invalid_property_value($message = null)
     {
         if ($message == null)
         {
-            $message = "Invalid property value.";
+            $message = self::$messages[self::INVALID_PROPERTY_VALUE];
         }
         return new self($message, self::INVALID_PROPERTY_VALUE);
     }
 
     public static function invalid_property()
     {
-        return new self("Invalid property.", self::INVALID_PROPERTY);
+        return new self(self::$messages[self::INVALID_PROPERTY], self::INVALID_PROPERTY);
     }
 
     public static function user_data($message = 'Unknown error')
@@ -155,99 +187,45 @@ class exception extends base_exception
 
     public static function object_deleted()
     {
-        return new self("Object deleted.", self::OBJECT_DELETED);
+        return new self(self::$messages[self::OBJECT_DELETED], self::OBJECT_DELETED);
     }
 
     public static function object_purged()
     {
-        return new self("Object purged.", self::OBJECT_PURGED);
+        return new self(self::$messages[self::OBJECT_PURGED], self::OBJECT_PURGED);
     }
 
     public static function object_exported()
     {
-        return new self("Object already exported.", self::OBJECT_EXPORTED);
+        return new self(self::$messages[self::OBJECT_EXPORTED], self::OBJECT_EXPORTED);
     }
 
     public static function object_imported()
     {
-        return new self("Object already imported.", self::OBJECT_IMPORTED);
+        return new self(self::$messages[self::OBJECT_IMPORTED], self::OBJECT_IMPORTED);
     }
 
     public static function missed_dependence()
     {
-        return new self("Missed dependence for object.", self::MISSED_DEPENDENCE);
+        return new self(self::$messages[self::MISSED_DEPENDENCE], self::MISSED_DEPENDENCE);
     }
 
     public static function tree_is_circular()
     {
-        return new self("Circular reference found in object's tree.", self::TREE_IS_CIRCULAR);
+        return new self(self::$messages[self::TREE_IS_CIRCULAR], self::TREE_IS_CIRCULAR);
     }
 
     public static function object_is_locked()
     {
-        return new self("Object is locked", self::OBJECT_IS_LOCKED);
+        return new self(self::$messages[self::OBJECT_IS_LOCKED], self::OBJECT_IS_LOCKED);
     }
 
     public static function get_error_string($code)
     {
-        switch ($code)
+        if (!array_key_exists($code, self::$messages))
         {
-            case exception::OK:
-                return "MGD_ERR_OK";
-            case exception::ACCESS_DENIED:
-                return "Access Denied.";
-            case exception::NO_METADATA:
-                return "Metadata class not defined.";
-            case exception::NOT_OBJECT:
-                return "Not Midgard Object.";
-            case exception::NOT_EXISTS:
-                return "Object does not exist.";
-            case exception::INVALID_NAME:
-                return "Invalid characters in object's name.";
-            case exception::DUPLICATE:
-                return "Object already exist.";
-            case exception::HAS_DEPENDANTS:
-                return "Object has dependants.";
-            case exception::RANGE:
-                return "Date range error.";
-            case exception::NOT_CONNECTED:
-                return "Not connected to the Midgard database.";
-            case exception::SG_NOTFOUND:
-                return "Sitegroup not found.";
-            case exception::INVALID_OBJECT:
-                return "Object not registered as Midgard Object.";
-            case exception::QUOTA:
-                return "Quota limit reached.";
-            case exception::INTERNAL:
-                return "Critical internal error. ";
-            case exception::OBJECT_NAME_EXISTS:
-                return "Object with such name exists in tree.";
-            case exception::OBJECT_NO_STORAGE:
-                return "Storage table not defined for object.";
-            case exception::OBJECT_NO_PARENT:
-                return "Parent object in tree not defined.";
-            case exception::INVALID_PROPERTY_VALUE:
-                return "Invalid property value.";
-            case exception::INVALID_PROPERTY:
-                return "Invalid property.";
-            case exception::USER_DATA:
-                return "";
-            case exception::OBJECT_DELETED:
-                return "Object deleted.";
-            case exception::OBJECT_PURGED:
-                return "Object purged.";
-            case exception::OBJECT_EXPORTED:
-                return "Object already exported.";
-            case exception::OBJECT_IMPORTED:
-                return "Object already imported.";
-            case exception::MISSED_DEPENDENCE:
-                return "Missed dependence for object.";
-            case exception::TREE_IS_CIRCULAR:
-                return "Circular reference found in object's tree.";
-            case exception::OBJECT_IS_LOCKED:
-                return "Object is locked";
-            default:
-                return "Undefined error";
+            return "Undefined error";
         }
+        return self::$messages[$code];
     }
 }
