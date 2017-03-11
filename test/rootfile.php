e755f8858b7f0c163bc3b4e4e5ad632797b9a6ac
flack <flack@contentcontrol-berlin.de>
2017-03-11 15:52:18 +0100

    Add forward compat to phpunit 6+

--- test/rootfile.php
+++ test/rootfile.php
@@ -1,4 +1,10 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 define('TESTDIR', __DIR__ . '/');
+
+if (   !class_exists('\PHPUnit_Framework_TestCase')
+    && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
+
 include 'testcase.php';
