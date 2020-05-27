b76ebc2d791dbc32853251f4c3685a747164f941
flack <flack@contentcontrol-berlin.de>
2020-05-27 19:34:17 +0200

    Add basic test

---
+++ test/mapping/classmetadataTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @copyright CONTENT CONTROL http://www.contentcontrol-berlin.de/
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License
+ */
+
+namespace midgard\portable\test\schema;
+
+use \midgard\portable\test\testcase;
+
+class classmetadataTest extends testcase
+{
+    public function test_get_schema_properties()
+    {
+        $cm = self::$em->getClassMetadata(self::$ns . '\\midgard_user');
+        $props = $cm->get_schema_properties();
+        $expected = ['id', 'login', 'password', 'active', 'authtype', 'authtypeid', 'usertype', 'person', 'guid', 'metadata'];
+        $this->assertEquals($expected, $props);
+
+        $props = $cm->get_schema_properties(true);
+        $this->assertEquals([], $props);
+    }
+}
